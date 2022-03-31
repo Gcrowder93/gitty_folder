8869577697d820d1ddb2e369f658a91571c6baec
Chase Crowder <gcrowder93@gmail.com>
2022-03-31 11:08:56 -0700

    updated tables in sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS post CASCADE;
+
+CREATE TABLE post (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL LIMIT 255
+);
+
+DROP TABLE IF EXISTS github_users;
+
+CREATE TABLE github_users (
+  - id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  - email TEXT NOT NULL
+  - password TEXT NOT NULL
+)
